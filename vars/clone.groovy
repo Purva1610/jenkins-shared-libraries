b2f4bc163621c919e015d39bf4abf8015579b1cb
Purva Kasumbiwal <purvakasumbiwal1610@gmail.com>
2026-05-09 01:13:01 +0530

    Refactor clone function to use call syntax

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,7 @@
-def(String url,String branch){
-  echo "code is cloning"
-  git url: "${url}", branch: "${branch}"
-  echo "code clonned successfully"
-  }
+def call(String url, String branch) {
+    echo "code is cloning ..."
+    
+    git url: url, branch: branch
+    
+    echo "code cloned successfully on branch "
+}
